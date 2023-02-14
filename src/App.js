d8bc9c774438d380f1dfdf6a4f9e885d62270a72
pujovic <pujovic@gmail.com>
2023-02-14 19:37:34 +0100

    First commit, final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import TaskList from "./components/Tasks/TaskList/TaskList";
+import TaskInput from "./components/Tasks/TaskInput/TaskInput";
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("to-do-tasks"))) {
+      setTasks(JSON.parse(localStorage.getItem("to-do-tasks")));
+    } else {
+      setTasks([]);
+    }
+  }, []);
+
+  const addTaskHandler = (enteredText) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      let id = Math.random().toString();
+      updatedTasks.unshift({ text: enteredText, id: id, done: false });
+      localStorage.setItem("to-do-tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+  };
+
+  const deleteItemHandler = (taskId) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.filter((task) => task.id !== taskId);
+      localStorage.setItem("to-do-tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+  };
+
+  const doneTaskHandler = (taskId) => {
+    setTasks((prevTasks) => {
+      const theDoneOne = prevTasks.findIndex((task) => task.id === taskId);
+      const updatedTasks = [...prevTasks];
+
+      if (updatedTasks[theDoneOne].done === false) {
+        updatedTasks[theDoneOne].done = true;
+      } else {
+        updatedTasks[theDoneOne].done = false;
+      }
+
+      localStorage.setItem("to-do-tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
+  };
+
+  let content = <p style={{ textAlign: "center" }}>No tasks yet.</p>;
+
+  if (tasks.length > 0) {
+    content = (
+      <TaskList
+        items={tasks}
+        onDeleteItem={deleteItemHandler}
+        onDoneTask={doneTaskHandler}
+      />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <section id="task-form">
+        <TaskInput onAddTask={addTaskHandler} />
+      </section>
+      <section id="tasks">{content}</section>
     </div>
   );
-}
+};
 
 export default App;
